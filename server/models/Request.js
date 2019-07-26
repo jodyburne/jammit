047cccd6b322fc29ddd3b69f685a6cfbedafe6d1
marianamv112 <marianamv112@gmail.com>
2019-07-26 16:53:35 +0100

    request crud routes

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,19 +1,23 @@
+userAccepted: [
+  {
+    accepted: { type: Boolean, default: false },
+  },
+]
 
-
- userAccepted: [ {, 
-                accepted: {type: Boolean, default: false}
-  }]
-
-  const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const advertSchema = new mongoose.Schema({
-      _postOwner:  { type: Schema.Types.ObjectId, ref: "User" },
-      _requester:  { type: Schema.Types.ObjectId, ref: "User" },
-      _post:       { type: Schema.Types.ObjectId, ref: "Advert"},
-      approval:    {type: String, enum: ["Approved","Denied","Pending"], default: false},
-      message: String,
-  })
+const requestSchema = new mongoose.Schema({
+  _postOwner: { type: Schema.Types.ObjectId, ref: 'User' },
+  _requester: { type: Schema.Types.ObjectId, ref: 'User' },
+  _post: { type: Schema.Types.ObjectId, ref: 'Advert' },
+  approval: {
+    type: String,
+    enum: ['Approved', 'Denied', 'Pending'],
+    default: 'Pending',
+  },
+  message: String,
+})
 
-const Advert = mongoose.model('Advert', advertSchema)
-module.exports = Advert
+const Request = mongoose.model('Request', requestSchema)
+module.exports = Request
--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,14 +1,26 @@
+const express = require('express')
 const Request = require('../models/Request')
 const { isLoggedIn } = require('../middlewares')
+const Advert = require('../models/Advert')
 const router = express.Router()
 
 /*This route is for creating requests*/
-router.get('/', isLoggedIn, (req, res, next) => {
-  let requestId = req.params.id
-  let approval = req.body
-  Request.findByIdAndUpdate(requestId, { approval: approval }, { new: true })
-    .then(request => {
-      res.json(request)
+router.get('/create-request/:advertId', isLoggedIn, (req, res, next) => {
+  let advertId = req.params.advertId
+  let requester = req.user._id
+  let message = req.body.message
+  Advert.findById(advertId)
+    .then(advert => {
+      Request.create({
+        _postOwner: advert._user,
+        _requester: requester,
+        _post: advert,
+        message: message,
+      })
+        .then(request => {
+          res.json(request)
+        })
+        .catch(err => next(err))
     })
     .catch(err => next(err))
 })
@@ -16,6 +28,7 @@ router.get('/', isLoggedIn, (req, res, next) => {
 /*This route returns user incoming requests*/
 router.get('/incoming-requests', isLoggedIn, (req, res, next) => {
   let userId = req.user._id
+
   Request.find({ _postOwner: userId })
     .then(request => {
       res.json(request)
@@ -27,10 +40,20 @@ router.get('/incoming-requests', isLoggedIn, (req, res, next) => {
 router.put('/handle-requests/:id', isLoggedIn, (req, res, next) => {
   let requestId = req.params.id
   let approval = req.body
-  Request.findByIdAndUpdate(requestId, { approval: approval }, { new: true })
+  Request.findByIdAndUpdate(requestId, approval, { new: true })
     .then(request => {
       res.json(request)
     })
+    .catch(err => next(err))
+})
+
+/* This route is for deleting requests*/
+router.delete('/:id', (req, res, next) => {
+  let requestId = req.params.id
+  Request.findByIdAndDelete(requestId)
+    .then(request =>
+      res.json({ message: 'Request has been succesfully deleted' })
+    )
     .catch(err => next(err))
 })
 
